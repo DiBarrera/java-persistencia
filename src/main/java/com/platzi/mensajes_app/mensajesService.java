510bc251cb33dbf1108c5e29d46350b47c53d86c
Diego Barrera Jiménez <di_barrera@hotmail.com>
2021-06-25 07:50:05 -0500

    Updating Data

--- src/main/java/com/platzi/mensajes_app/mensajesService.java
+++ src/main/java/com/platzi/mensajes_app/mensajesService.java
@@ -38,7 +38,16 @@ public class mensajesService {
     }
     
     public static void editarMensaje() {
-    
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Actualiza el mensaje");
+        String mensaje = sc.nextLine();
+        System.out.println("Indica el ID del mensaje a actualizar");
+        int id_mensaje = sc.nextInt();
+        Mensajes actualizacion = new Mensajes();
+        actualizacion.setId_mensaje(id_mensaje);
+        actualizacion.setMensaje(mensaje);
+        MensajesDAO.actualizarMensajeDB(actualizacion);
+        
     }
     
 }
