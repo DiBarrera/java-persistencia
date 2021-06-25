8879fdb6952f6fa7468205571e354ff3a7a5ba6e
Diego Barrera Jiménez <di_barrera@hotmail.com>
2021-06-25 05:18:23 -0500

    Logic for application

---
+++ src/main/java/com/platzi/mensajes_app/MensajesDAO.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.platzi.mensajes_app;
+
+/**
+ *
+ * @author diegobarrera
+ */
+public class MensajesDAO {
+    
+    public static void crearMensajeDB(Mensajes mensaje) {
+    
+    }
+    
+    public static void leerMensajesDB() {
+    
+    }
+    
+    public static void borrarMensajeDB() {
+    
+    }
+    
+    public static void actualizarMensajeDB(Mensajes mensaje) {
+    
+    }
+}
--- src/main/java/com/platzi/mensajes_app/inicio.java
+++ src/main/java/com/platzi/mensajes_app/inicio.java
@@ -6,6 +6,7 @@
 package com.platzi.mensajes_app;
 
 import java.sql.Connection;
+import java.util.Scanner;
 
 /**
  *
@@ -15,6 +16,41 @@ public class inicio {
     
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+        
+        int opcion = 0;
+        
+        do {
+            System.out.println("---------------------------");
+            System.out.println(" Aplicacion de mensajes");
+            System.out.println(" 1. Crear mensaje");
+            System.out.println(" 2. Listar mensajes");
+            System.out.println(" 3. Editar mensaje");
+            System.out.println(" 4. Eliminar mensaje");
+            System.out.println(" 5. Salir");
+            // Leemos la opcion del usuario
+            opcion = sc.nextInt();
+            
+            switch(opcion) {
+                case 1:
+                    mensajesService.crearMensaje();
+                    break;
+                case 2:
+                    mensajesService.listarMensajes();
+                    break;
+                case 3:
+                    mensajesService.borrarMensaje();
+                    break;
+                case 4:
+                    mensajesService.editarMensaje();
+                    break;
+                default:
+                    break;
+            }
+            
+        } while(opcion != 5);
+        
+/*
         Conexion conexion = new Conexion();
         
         try(Connection cnx = conexion.get_connection()) {
@@ -22,5 +58,6 @@ public class inicio {
         } catch(Exception e) {
             System.out.println(e);
         }
+*/
     }
 }
